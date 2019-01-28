f82e0f9f4267e86cdc7a5541f35cd851bb65adf1
Jafar Ansari <jafaransari@Jafars-MacBook-Pro.local>
2019-01-28 23:30:44 +0530

    database relationship

--- app/Model/Review.php
+++ app/Model/Review.php
@@ -1,10 +1,13 @@
 <?php
 
 namespace App\Model;
-
+use App\Model\Product;
 use Illuminate\Database\Eloquent\Model;
 
 class Review extends Model
 {
-    //
+    // relation between Review and product model
+    public function product(){
+      return $this->belongsTo(Product::class);
+    }
 }
